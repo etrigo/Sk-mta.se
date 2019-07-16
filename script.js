3da69de1d7c2f162da153b02ca7be60671456597
etrigo <bjorn.sjostedt@etrigo.com>
2019-07-16 09:19:41 +0200

    fixed copy funktion

--- script.js
+++ script.js
@@ -1,48 +1,17 @@
-// button
-/*window.onload = function() {
-  var jokerBox = document.getElementById("jokerBox");
-  var jokerButton = document.getElementById("jokerButton");
-  var jokerButtonText = document.getElementById("jokerButtonText");
-  
-  jokerButton.onclick = function() {
-    jokerBox.style.display = "block";
-    jokerButtonText.innerHTML = "igen!"
-  }
-}*/
-
-
-// button
-
 function joker() {
   document.getElementById("jokerBox").style.display = "block";
   document.getElementById("jokerButtonText").innerHTML = "igen!";
   }
 
-
-// copy url and get modal
-window.onload = function() {
-  // Get the modal
+function copyFunction() {
+  document.getElementById("siteUrl").select();
+  document.execCommand("copy");
   var modal = document.getElementById("myModal");
-
-  // Get the button that opens the modal
-  var urlField = document.getElementById("siteUrl");
-
-  // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("closeModal")[0];
-
-  // When the user clicks the button, open the modal 
-  urlField.onclick = function() {
-    urlField.select();
-    document.execCommand("copy");
-    modal.style.display = "block";
-  }
-
-  // When the user clicks on <span> (x), close the modal
+  modal.style.display = "block";
   span.onclick = function() {
     modal.style.display = "none";
   }
-
-  // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
