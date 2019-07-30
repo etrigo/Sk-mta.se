9e1438d33638fef46d3afb98bba866294b0481d1
etrigo <bjorn.sjostedt@live.com>
2019-07-30 22:56:44 +0200

    trying

--- dbconnect.php
+++ dbconnect.php
@@ -1,19 +1,13 @@
 <?php
-  require_once 'dbconfig_skamta.php';
+  require_once '../../dbconfig/dbconfig_skamta.php';
   try {
     $pdo = new PDO ("mysql:host=$host;dbname=$dbname;charset=$charset", $username, $password);
-    $sql = "SELECT jokeID, jokes, frequency
-            FROM jokes
-            ORDER BY RAND()
-            Limit 1";
+    $sql = 'SELECT jokes FROM jokes WHERE jokeID = 59;';
     $q = $pdo->query($sql);
     $q->setFetchMode(PDO::FETCH_ASSOC);
-    $row = $q->fetch();
-        $result = $row['jokeID'];
-        echo htmlspecialchars($row['jokes']);
-    $sqlup = "UPDATE jokes SET frequency = frequency + 1 WHERE jokeID = $result";
-    $q = $pdo->query($sqlup);
-    $q->setFetchMode(PDO::FETCH_ASSOC);
+    while ($row = $q->fetch()):
+        echo nl2br($row['jokes']);
+    endwhile;
   } 
   catch (PDOException $e) {
   die("Could not connect to the database $dbname :" . $e->getMessage());
