d31951592e0a00bbcb967ef61d8aaaba29c83248
etrigo <bjorn.sjostedt@live.com>
2019-08-02 18:56:32 +0200

    minor test stuff

--- dbconnect.php
+++ dbconnect.php
@@ -12,6 +12,10 @@
     $row = $q->fetch();
     
       echo nl2br(str_replace('\\n', "\n", $row['jokes']));
+
+      echo <buttonphp
+    // $jokeUrl = "<a href='index.html/?ID={$row['jokeID']}'>'länk'</a>";
+    
     
     $result = $row['jokeID'];
     $sqlup = "UPDATE jokes
